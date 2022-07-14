f1a00d23407a708c354f9916702a4632c6b46167
jboshin <109200788+jboshin@users.noreply.github.com>
2022-07-14 15:26:30 +0800

    Delete Data_cleansing directory

--- Data_cleansing/ComputerName_change.py
+++
@@ -1,5 +0,0 @@
-# 前為取代檔，後為新檔
-with open("1564140294_1765652.csv",encoding="utf-8") as f, open("computername.txt", 'w',encoding="utf-8") as n:
-    for line in f:
-        line=line.strip('\n')
-        n.write("<choice value=\""+line+"\">"+line+"</choice>\n")
